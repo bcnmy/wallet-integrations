3b9aad686358554ec1e65b2826034ab6d57adbe1
divya <divya@biconomy.io>
2020-06-04 16:48:32 +0530

    Eth transfer from Gnosis wallet using biconomy

--- gnosis/index.js
+++ gnosis/index.js
@@ -4,21 +4,19 @@ const Biconomy = require("@biconomy/mexa");
 const ethers = require('ethers');
 const Web3 = require('web3');
 const {abi} = require('./abi');
+const token = process.env.INFURA_TOKEN;
+const providerUrl = 'https://kovan.infura.io/v3/' + token;
+const privateKey = process.env.privateKey;
+const publicAddress = "<publicAddress>"
 
-
-/* Register Dapp onto biconomy dashboard to get the x-api-key
-*/
-const providerUrl = 'https://kovan.infura.io/v3/' + process.env.INFURA_TOKEN;
-const biconomy = new Biconomy(new Web3.providers.HttpProvider(providerUrl),{apiKey: "<x-api-Key>",debug:true});
+// Çreate web3
+const biconomy = new Biconomy(new Web3.providers.HttpProvider(providerUrl),
+{ apiKey: "<apiKey>", debug:true });
 const web3 = new Web3(biconomy);
 
-const privateKey = process.env.privateKey;
-const publicAddress = "<user_public_address>"
 const proxyFactoryAddress = '0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B'; // kovan
+const proxyAddress = "0xb16a5a4dbb3e837bf43e8bb8a699d99b7bef6943";
 const gnosisSafeAddress = '0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F'; // kovan
-const tokenAddress = '0xf676922BA0564B6925bC9142CB30938bAdDb9f18'; // SPK token
-const receiverAddress = '<receiver_address>';
-const value = '0.01';
 
 /**
  * Get the nonce of proxy contract
@@ -53,7 +51,6 @@ const createProxyContract = function(proxyFactoryAddress, gnosisSafeAddress) {
             0,
             '0x0000000000000000000000000000000000000000',
         ).encodeABI();
-        // console.log(creationData);
 
         // Create Proxy
         const proxyFactory = new web3.eth.Contract(abi.ProxyFactory, proxyFactoryAddress);
@@ -67,8 +64,6 @@ const createProxyContract = function(proxyFactoryAddress, gnosisSafeAddress) {
             "value": "0x0",
             "data": tx
         };
-
-        // console.log(txParams);
 
         const signedTx = await web3.eth.accounts.signTransaction(txParams, '0x'+privateKey);
         await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
@@ -101,14 +96,11 @@ const createProxyContract = function(proxyFactoryAddress, gnosisSafeAddress) {
  * @param {string} gnosisSafeAddress
  * @returns {string} tx hash
  */
-const executeTokenTx = async function(proxyAddress, to, destination, value, nonce, gnosisSafeAddress) {
+const executeTokenTx = async function(proxyAddress, to, value, nonce, gnosisSafeAddress) {
     const proxyContract = new web3.eth.Contract(abi.GnosisSafe, proxyAddress);
 
     // Set parameters of execTransaction()
-    const valueWei = web3.utils.toWei('0', 'ether'); // 0 ETH
-    const tokenContract = new web3.eth.Contract(abi.erc20Token, to);
-    const data = tokenContract.methods.transfer(destination, web3.utils.toWei(value, 'ether')).encodeABI(); // Encode data of token transfer()
-    console.log('Data payload:', data);
+    const valueWei = web3.utils.toWei(value, 'ether'); // 0.1 ETH
     const operation = 0; // CALL
     const gasPrice = 0; // If 0, then no refund to relayer
     const gasToken = '0x0000000000000000000000000000000000000000'; // ETH
@@ -117,21 +109,22 @@ const executeTokenTx = async function(proxyAddress, to, destination, value, nonc
     let txGasEstimate = 0
     try {
         const gnosisSafeMasterCopy = new web3.eth.Contract(abi.GnosisSafe, gnosisSafeAddress);
-        const estimateData = gnosisSafeMasterCopy.methods.requiredTxGas(to, valueWei, data, operation).encodeABI();
+        const estimateData = gnosisSafeMasterCopy.methods.requiredTxGas(to, valueWei, "0x0", operation).encodeABI();
 
         const estimateResponse = await web3.eth.call({to: proxyAddress, from: proxyAddress, data: estimateData, gasPrice: 0});
         txGasEstimate = new web3.utils.BN(estimateResponse.substring(138), 16);
         txGasEstimate = txGasEstimate.toNumber() + 10000; // Add 10k else we will fail in case of nested calls
         console.log("Safe Tx Gas estimate: " + txGasEstimate);
     } catch(e) {
-        console.log("Could not estimate gas");
+        console.log("Could not estimate gas:", 3);
     }
+
     // Get estimated base gas (Gas costs for that are indipendent of the transaction execution(e.g. base transaction fee, signature check, payment of the refund))
     let baseGasEstimate = 0; // If one of the owners executes this transaction it is not really required to set this (so it can be 0) TODO: if using erc20 token
     
     // Create typed data hash
     const transactionHash = await proxyContract.methods.getTransactionHash(
-        to, valueWei, data, operation, txGasEstimate, baseGasEstimate, gasPrice, gasToken, executor, nonce,
+        to.toLowerCase(), valueWei, "0x0", operation, txGasEstimate, baseGasEstimate, gasPrice, gasToken, executor, nonce,
     ).call();
 
     console.log('Transaction hash (typed data):', transactionHash);
@@ -146,9 +139,9 @@ const executeTokenTx = async function(proxyAddress, to, destination, value, nonc
     // Call proxy contract to execute Tx
     console.log('-----Execute Tx');
     const tx = await proxyContract.methods.execTransaction(
-        to, valueWei, data, operation, txGasEstimate, baseGasEstimate, gasPrice, gasToken, executor, newSignature,
+        to.toLowerCase(), valueWei, "0x0", operation, txGasEstimate, baseGasEstimate, gasPrice, gasToken, executor, newSignature,
     ).encodeABI();
-
+    
     let txParams = {
         "from": publicAddress,
         "gasLimit": web3.utils.toHex(210000),
@@ -157,13 +150,11 @@ const executeTokenTx = async function(proxyAddress, to, destination, value, nonc
         "data": tx
     };
 
-    // console.log(txParams);
-
-    const signedTx = await web3.eth.accounts.signTransaction(txParams, '0x'+privateKey);
+    const signedTx = await web3.eth.accounts.signTransaction(txParams, `<private_key>`);
     let txHash;
     let receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction, (error, _txHash)=>{
         if(error) {
-            return console.error(error);
+            return console.error("error");
         }
         txHash = _txHash;
     });
@@ -172,28 +163,45 @@ const executeTokenTx = async function(proxyAddress, to, destination, value, nonc
     return txHash;
 }
 
+const fixTrailingZero = (address) => {
+    let trailingZero = "";
+    for(let index = 0; index < (42-address.length); index++) trailingZero += "0";
+    return `${address.substring(0,2)}${trailingZero}${address.substring(2,address.length)}`;
+}
+
+const hexStripZeros = (value) => {
+    if (!web3.utils.isHex(value)) {
+        throw new Error('invalid hex string', { arg: 'value', value: value });
+    }
+    while (value.length > 3 && value.substring(0, 3) === '0x0') {
+        value = '0x' + value.substring(3);
+    }
+    return value;
+}
+
 biconomy.onEvent(biconomy.READY, async() => {
     // Initialize your dapp here like getting user accounts etc
     console.log("biconomy initialized");
 
-    // create proxy
-    // let proxyAddress = await createProxyContract(proxyFactoryAddress, gnosisSafeAddress)
+    // to create proxy: uncomment and run this line
+    // let proxyAddress = await createProxyContract(proxyFactoryAddress, gnosisSafeAddress);
+    
+    console.log(`proxy created ${proxyAddress}`);
 
     // Get current nonce
     const nonce = await getProxyContractNonce(proxyAddress);
+    console.log('Nonce:', nonce);
 
-    // Execute tx 
+    // Execute tx to withdraw 0.1 MK7
     const txHash = await executeTokenTx(
         proxyAddress,
-        tokenAddress,
-        receiverAddress,
-        value,
+        '<destination_address>', // address of the receiver
+        '0.01',
         nonce,
         gnosisSafeAddress,
     );
-    console.log('Withdraw <value> erc20 token:', txHash);
+    console.log('Withdraw 0.01 Eth:', txHash);
 
 }).onEvent(biconomy.ERROR, (error, message) => {
     console.log(error,message)
-    // Handle error while initializing mexa
-  });
+});
